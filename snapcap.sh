5cca00bcaf87e70f8a922b9bb03e80b2bd2f4ae2
Jason Stephens <jstephens@amyris.com>
2021-03-27 14:05:38 -0700

    Clean all functions deletes  both snaps and frames

--- snapcap.sh
+++ snapcap.sh
@@ -33,4 +33,27 @@ snap_clean_frames () {
 }
 
 
-# Clean the local ./frames/ directory
+# Clean ALL local ./frames/ director
+snap_clean_all () {
+    # Clean the local ./frames/ and ./snaps/ directories
+    echo ""
+    treela ${JAYCI_WORKSPACE}/snapcap/snapcap_util/frames/ 
+    rm -rf ${JAYCI_WORKSPACE}/snapcap/snapcap_util/frames/* || true 
+    echo ""
+    echo "Deleting frames from snapcap/snapcap_util/frames/"
+    echo ""
+    treela ${JAYCI_WORKSPACE}/snapcap/snapcap_util/frames/ 
+    echo "frames deleted"
+    echo ""
+
+    echo ""
+    treela ${JAYCI_WORKSPACE}/snapcap/snapcap_util/snaps/ 
+    rm -rf ${JAYCI_WORKSPACE}/snapcap/snapcap_util/snaps/* || true 
+    echo ""
+    echo "Deleting snaps from snapcap/snapcap_util/snaps/"
+    echo ""
+    treela ${JAYCI_WORKSPACE}/snapcap/snapcap_util/snaps/ 
+    echo "snaps deleted"
+    echo ""
+}
+
